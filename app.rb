54a06199401511842494a512c21064200ba69bbd
Ty-Zeta <tyofzeta@gmail.com>
2017-08-21 13:51:41 -0400

    not finished, ending up with multidimensional array on final page w/ just 1st element of each array

--- app.rb
+++ app.rb
@@ -9,11 +9,31 @@ post '/user_given_names' do
     name = params[:name]
     pairs = params[:pairs]
     pairs = pairing(name)
+    p "1 #{pairs}"
     erb :pairs_page, :locals => {:name => name, :pairs => pairs}
+
 end
 
 get '/pairs' do
     name = params[:name]
     pairs = params[:pairs]
+    p "2 #{pairs}"
     erb :pairs_page, :locals => {:name => name, :pairs => pairs}
+    
+end
+
+post '/pairs_page' do
+    name = params[:name]
+    pairs = params[:pairs]
+    p "3 #{pairs}"
+    erb :results, :locals => {:name => name, :pairs => pairs}
+    
+end
+
+get '/results' do
+    name = params[:name]
+    pairs = params[:pairs]
+    p "4 #{pairs}"
+    erb :results, :locals => {:name => name, :pairs => pairs}
+   
 end
