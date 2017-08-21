3c55322b3645edd8268afadacfe0691632158a3c
Marvin Laucher <marv070@gmail.com>
2017-08-21 15:28:07 -0400

    updated gemfile for deploy

--- app.rb
+++ app.rb
@@ -7,33 +7,37 @@ end
 
 post '/user_given_names' do
     name = params[:name]
-    pairs = params[:pairs]
+    # pairs = params[:pairs]
     pairs = pairing(name)
     p "1 #{pairs}"
-    erb :pairs_page, :locals => {:name => name, :pairs => pairs}
+    "what is returned to pairs #{pairs}"
+
+     erb :pairs_page, :locals => {:pairs => pairs}
 
 end
 
-get '/pairs' do
-    name = params[:name]
-    pairs = params[:pairs]
-    p "2 #{pairs}"
-    erb :pairs_page, :locals => {:name => name, :pairs => pairs}
+# get '/pairs' do
+#     name = params[:name]
+#     pairs = params[:pairs]
+#     p "2 #{pairs}"
+#     erb :pairs_page, :locals => {:name => name, :pairs => pairs}
     
-end
+# end
 
 post '/pairs_page' do
-    name = params[:name]
+    # name = params[:name]
     pairs = params[:pairs]
     p "3 #{pairs}"
-    erb :results, :locals => {:name => name, :pairs => pairs}
+     "show pairs arr here: #{pairs}"
+     erb :results, :locals => {:pairs => pairs}
     
 end
 
 get '/results' do
-    name = params[:name]
+    # name = params[:name]
     pairs = params[:pairs]
     p "4 #{pairs}"
-    erb :results, :locals => {:name => name, :pairs => pairs}
+
+    # erb :results, :locals => {:name => name, :pairs => pairs}
    
 end
